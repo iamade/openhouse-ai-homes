7303e1879a76fc600139b6088dc6cad1b14b23dd
Adesegun Koiki <kamar3deen@gmail.com>
2023-12-31 00:27:14 +0100

    header section done

---
+++ src/components/Header.jsx
@@ -0,0 +1,26 @@
+import MenuItem from "./MenuItem";
+import Link from "next/link";
+import Image from "next/image";
+import { IoMdHome } from "react-icons/io";
+import { BsFillInfoCircleFill } from "react-icons/bs";
+
+export default function Header() {
+  return (
+    <div className="flex justify-between mx-2 max-w-6xl sm:mx-auto items-center py-6">
+          <div className="">
+        <Link href="/">
+          <h2 className="text-2xl">
+          <img className="h-10" src="openhouselogo.svg" alt="logo loading,,,"/>
+           
+          </h2>
+        </Link>
+      </div>
+
+      <div className="flex">
+        <MenuItem title="HOME" address="/" Icon={IoMdHome} />
+        <MenuItem title="ABOUT" address="/about" Icon={BsFillInfoCircleFill} />
+      </div>
+    
+    </div>
+  );
+}
