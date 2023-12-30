1e6d373d394c8844745d00c6d8ca4acb3e066820
Adesegun Koiki <kamar3deen@gmail.com>
2023-12-30 21:43:24 +0100

    first commit

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+## Node to use
+Node.js version >= v18.17.0 is required.
+
+
 This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
 ## Getting Started
---
+++ src/app/head.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Head() {
+  return (
+    <>
+    <title>Openhouse.ai React/Next Js 14</title>
+     <meta content="width=device-width, initial-scale=1" name="viewport"/>
+     <meta name="description" content="Openhouse.ai app created with Next Js 14" />
+     <link rel="icon" href="/favicon.ico"/>
+ </>
+  )
+}
